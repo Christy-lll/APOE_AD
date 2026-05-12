3f052c6345a2a1ebf6e270d24210903a758f9d3d
Christy Li <christy.li.3839@gmail.com>
2026-05-12 15:25:00 +1000

    Diverse cohort analysis

--- proc/DC-Frontal proteomic .R
+++ proc/DC-Frontal proteomic .R
@@ -16,37 +16,38 @@ assay_meta <- read.csv(here("raw", "DiverseCohorts", "AMP-AD_DiverseCohorts_assa
 
 # Preprocessing ----
 ## Metadata ----
-specimen_meta <- specimen_meta %>%
-  filter(specimenID %in% colnames(proteomic_dat),
-         specimenID %in% assay_meta$specimenID,
-         individualID %in% individual_meta$individualID) %>%
-  select(individualID, specimenID, tissue, assay) %>%
-  left_join(assay_meta %>% select(specimenID, batch), by = "specimenID") %>%
-  filter(!grepl("^emdp", batch)) # drop emdp batches to ensure one specimen per individual
+temp_meta <- specimen_meta %>%
+  filter(dataGenerationSite == "Emory", tissue == "dorsolateral prefrontal cortex") %>%
+  group_by(individualID) %>%
+  mutate(status = n() > 1) %>% # Emory is a priority if there's more than 1 entry per ID
+  filter(!status | (status & grepl("^emdp", specimenID))) %>%
+  ungroup() %>%
+  select(individualID, specimenID)
 
-meta_dat <- assay_meta %>%
-  filter(specimenID %in% specimen_meta$specimenID,
-         isAssayControl == FALSE) %>%
-  select(specimenID, platform, TMTType) %>%
-  inner_join(specimen_meta, by = "specimenID") %>%
-  inner_join(individual_meta, by = "individualID") %>%
-  filter(ADoutcome %in% c("AD", "Control"),
-         ageDeath != "missing or unknown",
-         apoeGenotype != "missing or unknown") %>%
-  mutate(apoe4 = factor(grepl("4", apoeGenotype), labels = c("APOE4-", "APOE4+")),
+meta_dat <- individual_meta %>%
+  select(individualID, sex, race, ageDeath, apoeGenotype, ADoutcome) %>%
+  mutate(apoeGenotype = na_if(apoeGenotype, "missing or unknown"),
+         ageDeath = na_if(ageDeath, "missing or unknown")) %>%
+  filter(!is.na(apoeGenotype), !is.na(ageDeath), ADoutcome %in% c("AD", "Control")) %>%
+  inner_join(temp_meta, by = "individualID") %>%
+  inner_join(assay_meta[, c("specimenID", "batch")], by = "specimenID") %>%
+  mutate(apoe4 = factor(apoeGenotype %in% c("24", "34", "44"), labels = c("APOE4-", "APOE4+")),
          apoeGenotype = factor(apoeGenotype, levels = c("22", "23", "33", "24", "34", "44")),
-         age_death_90plus = as.integer(ageDeath == "90+"),
          age_death_num = as.numeric(if_else(ageDeath == "90+", "90", ageDeath)),
          diag_apoe = factor(paste0(apoe4, " ", ADoutcome),
                             levels = c("APOE4- Control", "APOE4- AD",
                                        "APOE4+ Control", "APOE4+ AD")))
+
+# any(duplicated(meta_dat$individualID))
+rm(temp_meta, individual_meta, assay_meta, specimen_meta)
+
 
 ## Proteomic data ----
 expr_mat <- proteomic_dat %>% 
   select(all_of(meta_dat$specimenID))
 # any(expr_mat == 0, na.rm = TRUE) --> no 0
 
-# median imputation 
+# median imputation & log transformation
 median_impute <- function(x) {
   x[is.na(x)] <- median(x, na.rm = TRUE)
   x
@@ -57,18 +58,20 @@ expr_mat <- expr_mat %>%
   apply(1, median_impute) %>% 
   t() 
 
-# log-transform
 expr_mat <- log2(expr_mat) 
 
 # boxplot(expr_mat, las = 2, cex.axis = 0.5, outline = FALSE)
-# final dim: 8703 proteins x 763 individuals
+# final dim: 8426 proteins x 779 individuals
 # all(colnames(expr_mat) == meta_dat$specimenID)
 
+## Save cleaned data----
+# saveRDS(expr_mat,  here("raw", "DiverseCohorts", "dlpfc_prot_expr.rds"))
+# saveRDS(meta_dat, here("raw", "DiverseCohorts", "dlpfc_prot_meta.rds"))
 
 # Demographic table ----
 library(gtsummary)
 demographic <- meta_dat %>%
-  select(diag_apoe, age_death_num, age_death_90plus, sex, apoeGenotype) %>%
+  select(diag_apoe, age_death_num, sex, apoeGenotype) %>%
   tbl_summary(
     by = diag_apoe,
     statistic = list(
@@ -77,8 +80,7 @@ demographic <- meta_dat %>%
     ),
     digits = all_continuous() ~ 1,
     label = list(
-      age_death_num ~ "Age at Death (max 90)",
-      age_death_90plus ~ "Age over 90",
+      age_death_num ~ "Age at Death (top-coded 90)",
       sex ~ "Sex",
       apoeGenotype ~ "APOE genotype"
     ),
@@ -114,12 +116,11 @@ p_varpart <- variancePartition::plotVarPart(vp_fit, main = "Variance Partition: 
 p_varpart$data <- p_varpart$data %>%
   mutate(variable = factor_labels[as.character(variable)])
 
-# Batch-diagnosis/apoe4 confounding check
 meta_dat %>%
-  group_by(batch, diag_apoe) %>%
+  group_by(batch, apoe4) %>%
   summarise(n = n()) %>%
-  pivot_wider(names_from = diag_apoe, values_from = n, values_fill = 0) %>%
-  print(n = 63)
+  pivot_wider(names_from = apoe4, values_from = n, values_fill = 0) %>%
+  print(n = 72)
 
 # write.csv(vp_medians, file.path(table_output_dir, "DC_frontal_varpart_medians.csv"), row.names = FALSE)
 # ggsave(file.path(pic_output_dir, "DC_frontal_varpart_violin.png"), p_varpart, width = 7, height = 5)
@@ -128,30 +129,31 @@ meta_dat %>%
 # Limma analysis ----
 ## Model: APOE4 effect adjusted for diagnosis, sex, age and batch ----
 library(limma)
-design_mat <- model.matrix(~ apoe4 + ADoutcome + sex + age_death_num + age_death_90plus + batch , 
+design_mat <- model.matrix(~ apoe4 + ADoutcome + sex + age_death_num + batch, 
                            data = meta_dat) # adjust for diagnosis, sex, age, and batch
 
-expr_mat <- expr_mat[rownames(expr_mat) != "APOE4|APOE4", ] # APOE4 is the top DE protein
 fit <- lmFit(expr_mat, design_mat)
 fit <- eBayes(fit)
 
 p_val_threshold <- 0.05
-logfc_threshold <- 0.13
 
 res_limma <- topTable(fit, coef = "apoe4APOE4+", number = Inf) %>%
   rownames_to_column(var = "Protein") %>%
   mutate(negLog10FDR = -log10(adj.P.Val),
-         `Direction of change` = case_when(adj.P.Val < p_val_threshold & logFC > logfc_threshold ~ "Upregulated",
-                                           adj.P.Val < p_val_threshold & logFC < -logfc_threshold ~ "Downregulated",
+         `Direction of change` = case_when(adj.P.Val < p_val_threshold & logFC > 0 ~ "Upregulated",
+                                           adj.P.Val < p_val_threshold & logFC < 0 ~ "Downregulated",
                                            TRUE ~ "Not significant"))
 
 sig_res_limma <- res_limma %>%
-  filter(adj.P.Val < p_val_threshold, abs(logFC) > logfc_threshold) 
+  filter(adj.P.Val < p_val_threshold) 
 
 # write.csv(res_limma, file.path(table_output_dir, "DC_frontal_all_limma.csv"), row.names = FALSE)
 # write.csv(sig_res_limma, file.path(table_output_dir, "DC_frontal_limma.csv"), row.names = FALSE)
 
 ## Volcano plot ----
+res_limma <- res_limma %>% filter(Protein != "APOE4|APOE4") # APOE4 is top DEP and distort volcano plot
+sig_res_limma <- sig_res_limma %>% filter(Protein != "APOE4|APOE4")
+
 p_limma <- ggplot(res_limma, aes(x = logFC, y = negLog10FDR)) +
   geom_point(aes(color = `Direction of change`), alpha = 0.6, size = 1.2) +
   ggrepel::geom_text_repel(data = sig_res_limma,
@@ -161,13 +163,13 @@ p_limma <- ggplot(res_limma, aes(x = logFC, y = negLog10FDR)) +
   scale_color_manual(values = c("Upregulated" = "red",
                                 "Downregulated" = "blue",
                                 "Not significant" = "grey")) +
-  geom_vline(xintercept = c(-logfc_threshold, logfc_threshold), 
+  geom_vline(xintercept = 0, 
              linetype = "dashed", color = "grey") +
   geom_hline(yintercept = -log10(p_val_threshold), 
              linetype = "dashed", color = "grey") +
   labs(title = sprintf("%d Differentially Expressed dlPCF Proteins by APOE4 Status (n = %d)", 
-                       nrow(sig_res_limma), nrow(meta_dat)),
-       subtitle = "Thresholds: adj_p < 0.05 & |log2 FC| > 0.13",
+                       nrow(sig_res_limma) + 1, nrow(meta_dat)),
+       subtitle = "Thresholds: adj_p < 0.05 | APOE4 not shown on plot",
        x = "log2 fold change",
        y = "-log10 adjusted P",
        color = "Direction of change"
@@ -176,5 +178,3 @@ p_limma <- ggplot(res_limma, aes(x = logFC, y = negLog10FDR)) +
   theme(legend.position = "bottom")
 
 # ggsave(file.path(pic_output_dir, "DC_frontal_limma_volcano.png"), p_limma, width = 8, height = 6)
-
-
--- proc/DC-Temporal proteomic.R
+++ proc/DC-Temporal proteomic.R
@@ -20,7 +20,7 @@ specimen_meta <- specimen_meta %>%
   filter(specimenID %in% colnames(proteomic_dat),
          specimenID %in% assay_meta$specimenID,
          individualID %in% individual_meta$individualID) %>%
-  select(individualID, specimenID, tissue, assay) %>%
+  select(individualID, specimenID, tissue, assay, dataGenerationSite) %>% # all from Emory
   left_join(assay_meta %>% select(specimenID, batch), by = "specimenID") %>%
   filter(n() == 1, .by = individualID) # keep only 1 sample per individual
 
@@ -35,11 +35,13 @@ meta_dat <- assay_meta %>%
          apoeGenotype != "missing or unknown") %>%
   mutate(apoe4 = factor(grepl("4", apoeGenotype), labels = c("APOE4-", "APOE4+")),
          apoeGenotype = factor(apoeGenotype, levels = c("22", "23", "33", "24", "34", "44")),
-         age_death_90plus = as.integer(ageDeath == "90+"),
          age_death_num = as.numeric(if_else(ageDeath == "90+", "90", ageDeath)),
          diag_apoe = factor(paste0(apoe4, " ", ADoutcome),
                             levels = c("APOE4- Control", "APOE4- AD",
                                        "APOE4+ Control", "APOE4+ AD"))) 
+
+# any(duplicated(meta_dat$individualID))
+rm(individual_meta, assay_meta, specimen_meta)
 
 ## Proteomic data ----
 # filter sample & code 0 as NA
@@ -62,14 +64,17 @@ expr_mat <- expr_mat %>%
 expr_mat <- log2(expr_mat) 
 
 # boxplot(expr_mat, las = 2, cex.axis = 0.5, outline = FALSE)
-# final dim: 8628 proteins x 208 individuals
+# final dim: 8682 proteins x 208 individuals
 # all(colnames(expr_mat) == meta_dat$specimenID)
 
+## Save cleaned data----
+# saveRDS(expr_mat,  here("raw", "DiverseCohorts", "stg_prot_expr.rds"))
+# saveRDS(meta_dat, here("raw", "DiverseCohorts", "stg_prot_meta.rds"))
 
 # Demographic table ----
 library(gtsummary)
 demographic <- meta_dat %>%
-  select(diag_apoe, age_death_num, age_death_90plus, sex, apoeGenotype) %>%
+  select(diag_apoe, age_death_num, sex, apoeGenotype) %>%
   tbl_summary(
     by = diag_apoe,
     statistic = list(
@@ -78,8 +83,7 @@ demographic <- meta_dat %>%
     ),
     digits = all_continuous() ~ 1,
     label = list(
-      age_death_num ~ "Age at Death (max 90)",
-      age_death_90plus ~ "Age over 90",
+      age_death_num ~ "Age at Death (top-coded 90)",
       sex ~ "Sex",
       apoeGenotype ~ "APOE genotype"
     ),
@@ -118,9 +122,9 @@ p_varpart$data <- p_varpart$data %>%
 
 # Batch-diagnosis/apoe4 confounding check
 meta_dat %>%
-  group_by(batch, diag_apoe) %>%
+  group_by(batch, apoe4) %>%
   summarise(n = n()) %>%
-  pivot_wider(names_from = diag_apoe, values_from = n, values_fill = 0) %>%
+  pivot_wider(names_from = apoe4, values_from = n, values_fill = 0) %>%
   print(n = 19)
 
 # write.csv(vp_medians, file.path(table_output_dir, "DC_temporal_varpart_medians.csv"), row.names = FALSE)
@@ -130,21 +134,48 @@ meta_dat %>%
 # Limma analysis ----
 ## Model: APOE4 effect adjusted for diagnosis, sex, age and batch ----
 library(limma)
-design_mat <- model.matrix(~ apoe4 + ADoutcome + sex + age_death_num + age_death_90plus + batch , 
+design_mat <- model.matrix(~ apoe4 + ADoutcome + sex + age_death_num + batch , 
                            data = meta_dat) # adjust for diagnosis, sex, age, and batch
 
 fit <- lmFit(expr_mat, design_mat)
 fit <- eBayes(fit)
 
 p_val_threshold <- 0.05
-logfc_threshold <- 0.13
 
 res_limma <- topTable(fit, coef = "apoe4APOE4+", number = Inf) %>%
   rownames_to_column(var = "Protein") %>%
   mutate(negLog10FDR = -log10(adj.P.Val),
-         `Direction of change` = case_when(adj.P.Val < p_val_threshold & logFC > logfc_threshold ~ "Upregulated",
-                                           adj.P.Val < p_val_threshold & logFC < -logfc_threshold ~ "Downregulated",
+         `Direction of change` = case_when(adj.P.Val < p_val_threshold & logFC > 0 ~ "Upregulated",
+                                           adj.P.Val < p_val_threshold & logFC < 0 ~ "Downregulated",
                                            TRUE ~ "Not significant"))
+sig_res_limma <- res_limma %>%
+  filter(adj.P.Val < p_val_threshold) 
 
 # No significant results
 # write.csv(res_limma, file.path(table_output_dir, "DC_temporal_all_limma.csv"), row.names = FALSE)
+
+## Volcano plot ----
+p_limma <- ggplot(res_limma, aes(x = logFC, y = negLog10FDR)) +
+  geom_point(aes(color = `Direction of change`), alpha = 0.6, size = 1.2) +
+  ggrepel::geom_text_repel(data = sig_res_limma,
+                           aes(label = Protein),
+                           size = 3,
+                           color = "black") +
+  scale_color_manual(values = c("Upregulated" = "red",
+                                "Downregulated" = "blue",
+                                "Not significant" = "grey")) +
+  geom_vline(xintercept = 0, 
+             linetype = "dashed", color = "grey") +
+  geom_hline(yintercept = -log10(p_val_threshold), 
+             linetype = "dashed", color = "grey") +
+  labs(title = sprintf("%d Differentially Expressed STG Proteins by APOE4 Status (n = %d)", 
+                       nrow(sig_res_limma), nrow(meta_dat)),
+       subtitle = "Thresholds: adj_p < 0.05",
+       x = "log2 fold change",
+       y = "-log10 adjusted P",
+       color = "Direction of change"
+  ) +
+  theme_classic() +
+  theme(legend.position = "bottom")
+
+# ggsave(file.path(pic_output_dir, "DC_frontal_limma_volcano.png"), p_limma, width = 8, height = 6)
